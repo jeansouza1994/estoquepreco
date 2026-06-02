5dfdcf7f4a2b72101da1bfa29850d95e69e75004
Jean Souza <jeansouza199487@gmail.com>
2026-06-02 16:09:26 -0300

    feat: Cria filas no RabbitMQ

---
+++ src/main/java/com/microservico/estoquepreco/conections/RabbitMQConection.java
@@ -0,0 +1,51 @@
+package com.microservico.estoquepreco.conections;
+
+import com.microservico.estoquepreco.constantes.RabbitmqConstantes;
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConection {
+    private static final String EXCHANGE_NAME = "amq.direct";
+
+    private final AmqpAdmin amqpAdmin;
+
+    public RabbitMQConection(AmqpAdmin amqpAdmin) {
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    private Queue fila(String nomeFila) {
+        return new Queue(nomeFila, true, false, false);
+    }
+
+    private DirectExchange trocaDireta() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    private Binding relacionamento(Queue fila, DirectExchange troca) {
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, troca.getName(), fila.getName(), null);
+    }
+
+    @PostConstruct
+    private void adciona() {
+        Queue filaEstoque = this.fila(RabbitmqConstantes.FILA_ESTOQUE);
+        Queue filaPreco = this.fila(RabbitmqConstantes.FILA_PRECO);
+
+        DirectExchange troca = this.trocaDireta();
+
+        Binding ligacaoEstoque = this.relacionamento(filaEstoque, troca);
+        Binding ligacaoPreco = this.relacionamento(filaPreco, troca);
+
+        this.amqpAdmin.declareQueue(filaEstoque);
+        this.amqpAdmin.declareQueue(filaPreco);
+
+        this.amqpAdmin.declareExchange(troca);
+
+        this.amqpAdmin.declareBinding(ligacaoEstoque);
+        this.amqpAdmin.declareBinding(ligacaoPreco);
+    }
+}
